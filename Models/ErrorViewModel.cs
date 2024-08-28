0e2185711bc11d77e2560699a36a58c661e6f0c0
Qadoor-SH <124529771+Qadoor-SH@users.noreply.github.com>
2024-08-28 10:04:43 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ActualTeast.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ActualTeast.Models
+{
+    public class User: IdentityUser
+    {
+    }
+}
